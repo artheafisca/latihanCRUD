af734899e72df403d152cf0f5d8c333494d9f79f
artheafisca <alittafisca@gmail.com>
2022-12-08 10:03:59 +0700

    upload tugas backend

---
+++ Latihan-Backend/latihanbackend/src/main/java/com/example/latihanbackend/LatihanbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.latihanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihanbackendApplication.class, args);
+	}
+
+}
---
+++ Latihan-Backend/latihanbackend/src/test/java/com/example/latihanbackend/LatihanbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latihanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
